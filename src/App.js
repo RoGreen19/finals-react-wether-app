69ba6edafa3e940a1a565b19647c470b5fd8138c
RoGreen19 <rogreen1903@gmail.com>
2023-05-05 15:20:13 +0100

    Create WeatherComponent files (js and css).

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import WeatherComponent from "./WeatherComponent";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <WeatherComponent />
       </div>
     </div>
   );
---
+++ src/WeatherComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./WeatherComponent.css";
+
+export default function WeatherComponent() {
+  return (
+    <div className="WeatherComponent">
+      <h1>Kyiv</h1>
+    </div>
+  );
+}
