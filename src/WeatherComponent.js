a44c584dc1fa650a119d3c74edcfd2bc7f418780
RoGreen19 <rogreen1903@gmail.com>
2023-05-05 19:43:51 +0100

    Debugg WeatherComponent.
    Add alt description to img teg. Change "class" to "className"

--- src/WeatherComponent.js
+++ src/WeatherComponent.js
@@ -4,47 +4,47 @@ import "./WeatherComponent.css";
 export default function WeatherComponent() {
   return (
     <div className="WeatherComponent">
-      <div class="card first">
-        <div class="card-header">Weather</div>
-        <div class="card text-bg-dark">
-          <img src="./cut2.jpg" class="card-img" alt="..." />
-          <div class="card-img-overlay">
+      <div className="card first">
+        <div className="card-header">Weather</div>
+        <div className="card text-bg-dark">
+          <img src="./cut2.jpg" className="card-img" alt="" />
+          <div className="card-img-overlay">
             <h3>Kyiv</h3>
-            <h6 class="date">25.11.2022</h6>
-            <h6 class="time">23.13</h6>
-            <h6 class="current-precipitation">Cloudy</h6>
+            <h6 className="date">25.11.2022</h6>
+            <h6 className="time">23.13</h6>
+            <h6 className="current-precipitation">Cloudy</h6>
             <form id="city-input">
               <input
-                class="search-line"
+                className="search-line"
                 type="search"
                 placeholder="Search"
                 id="search-new-city"
               />
-              <input class="search-button" type="Submit" value="🔍" />
+              <input className="search-button" type="Submit" placeholder="🔍" />
             </form>
-            <button class="current-city-button">Current</button>
-            <button class="celsius-button">°C</button>
-            <button class="fahrenheit-button">°F</button>
+            <button className="current-city-button">Current</button>
+            <button className="celsius-button">°C</button>
+            <button className="fahrenheit-button">°F</button>
             <img
-              class="main-weather-picture"
+              className="main-weather-picture"
               src="./rainy.png"
               alt="weather picture"
             />
             <h1>13°</h1>
 
-            <div class="card-footer">
-              <div class="row">
-                <div class="col weather-humidity">
+            <div className="card-footer">
+              <div className="row">
+                <div className="col weather-humidity">
                   <p>Humidity</p>
-                  <h4 class="humidity">100 %</h4>
+                  <h4 className="humidity">100 %</h4>
                 </div>
-                <div class="col weather-visibility">
+                <div className="col weather-visibility">
                   <p>Visibility</p>
-                  <h4 class="visibility">7.2 km</h4>
+                  <h4 className="visibility">7.2 km</h4>
                 </div>
-                <div class="col weather-wind">
+                <div className="col weather-wind">
                   <p>Wind</p>
-                  <h4 class="wind">10 km/h</h4>
+                  <h4 className="wind">10 km/h</h4>
                 </div>
               </div>
             </div>
@@ -53,46 +53,64 @@ export default function WeatherComponent() {
       </div>
 
       <br />
-      <div class="card second">
-        <div class="card-header">Week temperature</div>
-        <div class="container week-temperature">
-          <div class="row">
-            <div class="col">
+      <div className="card second">
+        <div className="card-header">Week temperature</div>
+        <div className="container week-temperature">
+          <div className="row">
+            <div className="col">
               <h5>24.11</h5>
-              <samp class="week-date">Mon</samp>
+              <samp className="week-date">Mon</samp>
               <br />
-              <img src="./cloudy.png" class="week-weather-icon" />
+              <img src="./cloudy.png" className="week-weather-icon" alt="" />
               <br />
               -12°С
             </div>
-            <div class="col">
+            <div className="col">
               <h5>25.11</h5>
-              <samp class="week-date">Tues</samp>
+              <samp className="week-date">Tues</samp>
               <br />
-              <img src="./rainy.png" class="week-weather-icon" /> <br />
+              <img
+                src="./rainy.png"
+                className="week-weather-icon"
+                alt=""
+              />{" "}
+              <br />
               -12°С
             </div>
-            <div class="col">
+            <div className="col">
               <h5>26.11</h5>
-              <samp class="week-date">Wed</samp>
+              <samp className="week-date">Wed</samp>
               <br />
-              <img src="./rainythunder.png" class="week-weather-icon" />
+              <img
+                src="./rainythunder.png"
+                className="week-weather-icon"
+                alt=""
+              />
               <br />
               +12°С
             </div>
-            <div class="col">
+            <div className="col">
               <h5>27.11</h5>
-              <samp class="week-date">Thurs</samp>
+              <samp className="week-date">Thurs</samp>
               <br />
-              <img src="./thnderstorm.png" class="week-weather-icon" />
+              <img
+                src="./thnderstorm.png"
+                className="week-weather-icon"
+                alt=""
+              />
               <br />
               +12°С
             </div>
-            <div class="col">
+            <div className="col">
               <h5>28.11</h5>
-              <samp class="week-date">Fri</samp>
+              <samp className="week-date">Fri</samp>
               <br />
-              <img src="./snowy.png" class="week-weather-icon" /> <br />
+              <img
+                src="./snowy.png"
+                className="week-weather-icon"
+                alt=""
+              />{" "}
+              <br />
               +14°С
             </div>
           </div>
