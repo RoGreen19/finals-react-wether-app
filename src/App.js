322c4c1110a228c83373e32782868f91213f432e
RoGreen19 <rogreen1903@gmail.com>
2023-05-05 15:06:21 +0100

    Download bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
+      </div>
     </div>
   );
 }
