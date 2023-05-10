6506e9253d0d24d8c3c8a49b8cf25c1e2efc882c
RoGreen19 <rogreen1903@gmail.com>
2023-05-10 19:01:03 +0100

    Debugging if alse statement in time and Day components

--- src/FormatedDate.js
+++ src/FormatedDate.js
@@ -4,11 +4,17 @@ export default function FormatedDate(props) {
   let date = props.date.getDate();
   if (date < 10) {
     date = `0${date}`;
+  } else {
+    date = `${date}`;
   }
+
   let month = props.date.getMonth() + 1;
   if (month < 10) {
     month = `0${month}`;
+  } else {
+    month = `${month}`;
   }
+
   let year = props.date.getFullYear();
   let todayDate = `${date}.${month}.${year}`;
 
--- src/FormatedTime.js
+++ src/FormatedTime.js
@@ -4,10 +4,15 @@ export default function FormatedTime(props) {
   let hours = props.time.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
+  } else {
+    hours = `${hours}`;
   }
+
   let minutes = props.time.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
+  } else {
+    minutes = `${minutes}`;
   }
 
   let currentTime = `${hours}:${minutes}`;
