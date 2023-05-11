a86b9e458aea9b8bd1d847502bf326496807536a
RoGreen19 <rogreen1903@gmail.com>
2023-05-11 21:16:53 +0100

    Update WeatherForecastDay.js

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherForecastDay(props) {
   let iconUrl = `https://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`;
@@ -50,57 +50,17 @@ export default function WeatherForecastDay(props) {
     return days[dayOfWeek];
   }
 
-  const [unit, setUnit] = useState("celsius");
-  function showFarenheit(event) {
-    event.preventDefault();
-    setUnit(`farenheit`);
-  }
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit(`celsius`);
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherForecast">
-        <h5>
-          {day()}.{month()}
-        </h5>
-        <samp className="week-date">{dayOfWeek()}</samp>
-        <br />
-        <img
-          src={iconUrl}
-          className="week-weather-icon"
-          alt="weatherForecast"
-        />
-        <br />
-        <span className="forecast-temperature-max">{maxTemperature()}/</span>
-        <span className="forecast-temperature-min">{minTemperature()}</span>
-        <button className="fahrenheit-button" onClick={showFarenheit}>
-          °F
-        </button>
-      </div>
-    );
-  } else {
-    return (
-      <div className="WeatherForecast">
-        <h5>
-          {day()}.{month()}
-        </h5>
-        <samp className="week-date">{dayOfWeek()}</samp>
-        <br />
-        <img
-          src={iconUrl}
-          className="week-weather-icon"
-          alt="weatherForecast"
-        />
-        <br />
-        <span className="forecast-temperature-max">{maxTemperature()}/</span>
-        <span className="forecast-temperature-min">{minTemperature()}</span>
-        <button className="celsius-button" onClick={showCelsius}>
-          °C
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherForecast">
+      <h5>
+        {day()}.{month()}
+      </h5>
+      <samp className="week-date">{dayOfWeek()}</samp>
+      <br />
+      <img src={iconUrl} className="week-weather-icon" alt="weatherForecast" />
+      <br />
+      <span className="forecast-temperature-max">{maxTemperature()}/</span>
+      <span className="forecast-temperature-min">{minTemperature()}</span>
+    </div>
+  );
 }
